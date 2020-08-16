9fb7f0085cb115a71d44b0c0e1f7ced8bbb903e4
Paul Thompson <pmt10@case.edu>
2020-08-16 18:06:18 -0400

    add linux compile script

---
+++ Build/Linux/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+g++ -c -fPIC -I/opt/pylon5/include ../../src/BaslerCpp.cpp -lpthread
+
+g++ -c -fPIC -I/opt/pylon5/include ../../src/BaslerCWrapper.cc -o BaslerCWrapper.o
+
+g++ BaslerCpp.o BaslerCWrapper.o -shared -o BaslerCpp.so -Wl,--enable-new-dtags -Wl,-rpath,/opt/pylon5/lib64 -L/opt/pylon5/lib64 -Wl,-E -lpylonbase -lpylonutility -lGenApi_gcc_v3_1_Basler_pylon_v5_1 -lGCBase_gcc_v3_1_Basler_pylon_v5_1
